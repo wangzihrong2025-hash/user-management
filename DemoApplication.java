291ab21eebd88780e499fd3a37c5bb693c900147
ワンツロン <wangzihrong2025@gmail.com>
2026-03-31 23:53:03 +0900

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -1,12 +1,13 @@
 #DemoUserManagement
 
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-    public class DemoApplication {
-        public static void main(String[] args) { 
-SpringApplication.run(DemoApplication.class,
-args);
-    } 
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
